4d493fbcc0b5b59aa851ebe7f2ea9668465de4d7
HalitYasin <halityasinsut@gmail.com>
2023-05-26 16:02:01 +0300

    Added a new level and more
     - Changed the name of the first level from "Sandbox" to "Over"
     - Added a new level "Under"
     - Increased rotation speed of the rocket
     - Modified CollisionHandler
     - Players move on to the next level if rocket reaches landing pad
     - Player can't control rocket if rocket collided with an obstacle or landing pad
     - Added a delay to level loading and reloading

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float reloadLevelDelay = 1f;
+    [SerializeField] float nextLevelDelay = 1f;
+
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -10,20 +13,46 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("Friendly");
                 break;
-            case "Fuel":
-                Debug.Log("Fuel");
-                break;
             case "Finish":
                 Debug.Log("Finish");
+                StartFinishSequence();
                 break;
             default:
                 Debug.Log("You've exploded!");
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
     }
 
-    private static void ReloadLevel()
+    void StartCrashSequence()
+    {
+        // todo add SFX upon crash
+        // todo add particle effect upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", reloadLevelDelay);
+    }
+
+    void StartFinishSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", nextLevelDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        if (currentSceneIndex < sceneCount - 1)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
